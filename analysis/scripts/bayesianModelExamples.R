75258f3855215489f9b3a4e360c553b233b238ea
Josie Hughes <josie.hughes@canada.ca>
2023-12-15 17:11:18 -0500

    Tweaks, and adding simulation set without composition bias.

--- analysis/scripts/bayesianModelExamples.R
+++ analysis/scripts/bayesianModelExamples.R
@@ -7,8 +7,6 @@ library(RColorBrewer)
 
 baseDir <- "."
 
-simBig<-getSimsNational(replicates=3000,forceUpdate=T)
-
 monitoringScns = data.frame(obsYears=c(1,15),collarCount=c(0,30),cowMult=c(6),collarInterval=c(1),assessmentYrs=c(3))
 stateScns = data.frame(obsAnthroSlope=c(2),projAnthroSlope=c(2))
 stateScns = merge(stateScns,data.frame(rep=seq(1:1)))
@@ -33,8 +31,11 @@ eParsIn$freqStartsByYear <- data.frame(Year = 1981:2023,
 eParsIn$collarOnTime=1
 eParsIn$collarOffTime=12
 eParsIn$collarNumYears=1
-
 labFontSize = 10; breakInterval=5
+
+###############################
+#With bias adjustment
+simBig<-getSimsNational(replicates=3000,forceUpdate=T)
 
 priorResult = caribouMetrics:::runScnSet(scns[1,],eParsIn,simBig,getKSDists=F,printProgress=F)
 priorResult$obs.all=NULL
@@ -90,6 +91,106 @@ plts <- ggpubr::ggarrange(recPrior, survPrior, lambdaPrior,
 ggpubr::ggarrange(plts, leg, ncol = 2, widths = c(6,1))+bgcolor("white")
 
 ggsave(paste0(baseDir,"/analysis/paper/figs/bayesianModelExamples.png"),
+       width = 12*0.8, height = 3.6*2, units = "in",
+       dpi = 1200)
+
+###############################
+#Without bias adjustment
+
+biasPars = data.frame(qMax=0,uMax=0,zMax=0)
+simBig<-getSimsNational(replicates=3000,forceUpdate=T,cPars=getScenarioDefaults(paramTable=biasPars))
+
+scns=merge(subset(scns,select=setdiff(names(scns),names(biasPars))),biasPars)
+priorResult = caribouMetrics:::runScnSet(scns[1,],eParsIn,simBig,getKSDists=F,printProgress=F)
+head(priorResult$obs.all)
+
+priorResult$obs.all=NULL
+
+yr_scale1 <- scale_x_continuous(breaks = 2023:2072 %>% .[0:9*5+1],
+                                labels = 2009:2058 %>% .[0:9*5+1] )
+yr_scale2 <- scale_x_continuous(breaks = 2009:2058 %>% .[0:9*5+1],
+                                labels = 2009:2058 %>% .[0:9*5+1] )
+recPrior =  plotRes(priorResult, "Recruitment", lowBound=0, highBound = 0.75,
+                    legendPosition="none",breakInterval=breakInterval,
+                    labFontSize=labFontSize) +
+  yr_scale1 +
+  labs(tag = "a")
+plot(recPrior)
+survPrior =  plotRes(priorResult, "Adult female survival", lowBound=0.6,
+                     legendPosition="none",breakInterval=breakInterval,
+                     labFontSize=labFontSize)+
+  yr_scale1
+plot(survPrior)
+lambdaPrior =  plotRes(priorResult, "Population growth rate", lowBound=0.6,
+                       legendPosition="none",breakInterval=breakInterval,
+                       labFontSize=labFontSize)+
+  yr_scale1 +
+  ylim(c(0, 1.2))
+plot(lambdaPrior)
+
+posteriorResult = caribouMetrics:::runScnSet(scns[2,],eParsIn,simBig,getKSDists=F,printProgress=F)
+recPosterior =  plotRes(posteriorResult, "Recruitment", lowBound=0,highBound = 0.75,
+                        legendPosition="none",breakInterval=breakInterval,
+                        labFontSize=labFontSize)+
+  yr_scale2 +
+  labs(tag = "b")
+plot(recPosterior)
+survPosterior =  plotRes(posteriorResult, "Adult female survival", lowBound=0.6,
+                         legendPosition="none",breakInterval=breakInterval,
+                         labFontSize=labFontSize)+
+  yr_scale2
+plot(survPosterior)
+lambdaPosterior =  plotRes(posteriorResult, "Population growth rate", lowBound=0.6,
+                           legendPosition="none",breakInterval=breakInterval,
+                           labFontSize=labFontSize)+
+  yr_scale2 +
+  ylim(c(0, 1.2))
+plot(lambdaPosterior)
+
+leg <- plotRes(posteriorResult, "Recruitment", lowBound=0,highBound = 0.75,
+               legendPosition="left",breakInterval=breakInterval,labFontSize=labFontSize)
+leg <- ggpubr::get_legend(leg)
+
+# combine ggplots to one figure
+plts <- ggpubr::ggarrange(recPrior, survPrior, lambdaPrior,
+                          recPosterior, survPosterior, lambdaPosterior, labels = "",
+                          ncol = 3, nrow = 2, vjust = 1)
+ggpubr::ggarrange(plts, leg, ncol = 2, widths = c(6,1))+bgcolor("white")
+
+ggsave(paste0(baseDir,"/analysis/paper/figs/bayesianModelExamplesNoBias.png"),
+       width = 12*0.8, height = 3.6*2, units = "in",
+       dpi = 1200)
+
+###############################
+#Sensitivity of lambda to bias par adjustment
+
+#Interpretation - uncertainty is so high that the additional uncertainty associated with composition survey biases has little noticable effect on overall uncertainty.
+#However, impacts on mean are noticeable.
+biasPars = expand.grid(uMax=c(0,0.2,0.4),zMax=c(0,0.2,0.4))
+#biasPars$uMin= biasPars$uMax; biasPars$zMin=biasPars$zMax
+#biasPars = expand.grid(zMax=c(0.2,0.4),uMax=c(0))
+#biasPars$zMin[biasPars$zMax==0.2]= 0.2
+#biasPars$zMin[biasPars$zMax==0.4]= 0
+
+scnsN=merge(subset(scns[1,],select=setdiff(names(scns),names(biasPars))),biasPars)
+scnsN$projYears = 30
+priorResult = caribouMetrics:::runScnSet(scnsN,eParsIn,simBig,getKSDists=F,printProgress=F)
+
+priorResult$obs.all=NULL; priorResult$sim.all=NULL
+
+yr_scale1 <- scale_x_continuous(breaks = 2023:2072 %>% .[0:9*5+1],
+                                labels = 2009:2058 %>% .[0:9*5+1] )
+yr_scale2 <- scale_x_continuous(breaks = 2009:2058 %>% .[0:9*5+1],
+                                labels = 2009:2058 %>% .[0:9*5+1] )
+lambdaPrior1 =  plotRes(priorResult, "Population growth rate", lowBound=0.6,
+                       legendPosition="none",breakInterval=breakInterval,
+                       labFontSize=labFontSize,facetVars=c("uMax","zMax"))+
+  yr_scale1 +
+  ylim(c(0.75, 1.25))
+
+plot(lambdaPrior1)
+
+ggsave(paste0(baseDir,"/analysis/paper/figs/bayesianModelBiasSensitivity.png"),
        width = 12*0.8, height = 3.6*2, units = "in",
        dpi = 1200)
 
--- analysis/scripts/sensitivity1.R
+++ analysis/scripts/sensitivity1.R
@@ -3,19 +3,6 @@
 
 ########################
 #sensitivity
-#setName = "s1"
-#minimalScn = expand.grid(P=1,st=1,cmult=0,ri=c(1,4),assessmentYrs=3)
-#monitoringScns = expand.grid(P=c(1,2,4,8,16),st=c(30,60),cmult=c(2,3,4),ri=c(1,2,4),assessmentYrs=c(1,3))
-#stateScns = data.frame(tA=c(0,20,40,60,0,20,40,60),aS=c(0,1,1,1,0,0,0,0),aSf=c(1,1,1,1,0,0,0,0))
-#stateScns = merge(stateScns,data.frame(rep=seq(1:100)))
-
-#monitoringScns = expand.grid(P=c(1,4,16),st=c(60),cmult=c(3),ri=c(1),assessmentYrs=c(3))
-#stateScns = data.frame(tA=c(0,20,40,60),aS=c(0,1,1,1),aSf=c(1,1,1,1))
-#stateScns = merge(stateScns,data.frame(rep=seq(1:10)))
-
-#monitoringScns = expand.grid(P=c(1,4,8,16),st=c(30,60),cmult=c(3),ri=c(1,2,4),assessmentYrs=1)
-#stateScns = expand.grid(iA=c(0),rep=seq(1:30))
-
 setName = "s7"
 monitoringScns = expand.grid(obsYears=c(1,2,4,8,16,24),collarCount=c(0,15,30,60),
                              cowMult=c(3,6,9),collarInterval=c(1,4),
@@ -40,13 +27,36 @@ table(scns$repBatch)
 scns$N0 = 2000
 scns$adjustR = TRUE
 scns$preYears = max(scns$obsYears)-scns$obsYears
-#scns=scns[10,]
-#scResults = readRDS("temp.Rds")
-head(scns)
 
 scns$pageLab = paste0("cmult",scns$cowMult,"ay",scns$assessmentYrs,"aSf",scns$projAnthroSlope,"repBatch",scns$repBatch)
 scns$pageId = as.numeric(as.factor(scns$pageLab))
-unique(scns$pageId)
+
+write.csv(scns,paste0("tabs/",setName,".csv"),row.names=F)
+pages=unique(scns$pageLab)
+
+
+setName = "s8"
+stateScns = data.frame(tA=c(0,20,40,60),
+                       obsAnthroSlope=c(0,1,1,1),
+                       projAnthroSlope=c(1,1,1,1))
+stateScns = merge(stateScns,data.frame(rep=seq(1:500)))
+
+stateScns$sQuantile=runif(nrow(stateScns),min=0.01,max=0.99)
+stateScns$rQuantile = runif(nrow(stateScns),min=0.01,max=0.99)
+scns=merge(monitoringScns,stateScns)
+
+scns$qMax = 0;scns$uMax=0;scns$zMax=0
+scns$iAnthro = scns$tA-(scns$obsYears-1)*scns$obsAnthroSlope
+scns$projYears = 20
+unique(scns$iAnthro)
+scns$repBatch = ceiling(scns$rep/50)
+table(scns$repBatch)
+scns$N0 = 2000
+scns$adjustR = TRUE
+scns$preYears = max(scns$obsYears)-scns$obsYears
+
+scns$pageLab = paste0("cmult",scns$cowMult,"ay",scns$assessmentYrs,"aSf",scns$projAnthroSlope,"repBatch",scns$repBatch)
+scns$pageId = as.numeric(as.factor(scns$pageLab))
 
 nrow(scns)
 write.csv(scns,paste0("tabs/",setName,".csv"),row.names=F)
