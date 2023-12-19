9396779e35bf5b4d73a061ba433633a83112d02e
see24 <sarahendicott4@gmail.com>
2023-12-19 13:21:23 -0500

    modify for set 8

--- analysis/scripts/sensitivityMinimal.R
+++ analysis/scripts/sensitivityMinimal.R
@@ -6,7 +6,7 @@ cpageId <- commandArgs(trailingOnly = TRUE)
 #setwd("C:/Users/HughesJo/Documents/gitprojects/Caribou-Demographic-Projection-Paper")
 library(caribouMetrics)
 
-setName = "s7"
+setName = "s8"
 
 #######################
 dir.create(paste0("figs/",setName),recursive=T)
