34bd5decab80d4afb8b39178a382fd980bafadda
Hughes <Josie.Hughes@ec.gc.ca>
2024-10-18 09:28:07 -0400

    more prep for submission

--- analysis/scripts/3_EVSIPlots.R
+++ analysis/scripts/3_EVSIPlots.R
@@ -21,7 +21,7 @@ batchStrip<-function(l,batches=c(10,seq(1:9))){
   return(l)
 }
 
-setName = "s11"
+setName = "s10"
 probsSum = read.csv(here::here(paste0("tabs/EVsample",setName,".csv")),stringsAsFactors = F)
 
 probsSum$pageLabC = batchStrip(probsSum$pageLab)
